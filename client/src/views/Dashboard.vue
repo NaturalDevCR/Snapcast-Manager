9df4c1524a7d5054c25550c7ebca75a54217e68d
Josue <35145882+NaturalDevCR@users.noreply.github.com>
2026-03-16 08:51:29 -0600

    chore: bump version to 0.1.49

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -72,7 +72,7 @@ const handleUpdateNodeJs = async () => {
 };
 
 // Update this constant synchronously with the package.json version before release
-const version = 'v0.1.48';
+const version = 'v0.1.49';
 </script>
 
 <template>
