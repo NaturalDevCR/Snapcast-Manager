24166c9699d0ae0685a1cf3e0328c8ab2889a116
Josue <35145882+NaturalDevCR@users.noreply.github.com>
2026-03-18 00:43:37 -0600

    chore: bump version to v0.1.62 and finalize UI refinements

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -72,7 +72,7 @@ const handleUpdateNodeJs = async () => {
 };
 
 // Update this constant synchronously with the package.json version before release
-const version = 'v0.1.61';
+const version = 'v0.1.62';
 </script>
 
 <template>
